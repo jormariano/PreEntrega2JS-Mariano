4fb3e9195b7ae60791bac1c8c3dcfefa5fb7f09a
jormariano <jorgelina.mariano@gmail.com>
2023-03-08 23:30:37 -0300

    segunda pre

--- js/main.js
+++ js/main.js
@@ -69,4 +69,107 @@ function precio_minimo(precio_total){
 function precio_envio(){
     const Costo_Envio = 50
     return Costo_Envio + fotos_seleccionadas;
+}
+
+// SEGUNDA PRE ENTREGA
+
+/*
+let foto = {
+    pais: "Alemania",
+    ciudad: "Berlin",
+    tamanio: 40,
+}
+
+console.log("Pais: "+foto.pais+"\nCiudad: "+foto.ciudad+"\nTamanio: "+foto.tamanio)
+
+for (const propiedad in foto1) {
+    console.log(foto1[propiedad]);
+}
+
+*/
+
+class Foto {
+    constructor(pais, ciudad, precio) {
+        this.pais = pais;
+        this.ciudad = ciudad;
+        this.precio = precio;
+        this.vendido = false;
     }
+    lugar() {
+        console.log("Esta foto es en " + this.ciudad + ", " + this.pais);
+    }
+    descripcion() {
+        return "Foto en " + this.ciudad + ", " + this.pais + "; sale $" + this.precio;
+    }
+    vender() {
+        this.vendido = true;
+    }
+}
+
+const foto1 = new Foto("Alemania", "Berlin", 20);
+const foto2 = new Foto("Argentina", "Buenos Aires", 50);
+const foto3 = new Foto("Brasil", "Rio de Janeiro", 20);
+const foto4 = new Foto("Colombia", "Bogota", 50);
+const foto5 = new Foto("España", "Madrid", 40);
+
+foto1.lugar();
+foto2.lugar();
+foto3.lugar();
+foto4.lugar();
+foto5.lugar();
+
+let compras = ""
+let rta = ""
+
+do {
+    console.log("El muestrario se integra por: \n" + foto1.descripcion()+"\n" + foto2.descripcion()+"\n" + foto3.descripcion()+"\n" + foto4.descripcion()+"\n" + foto5.descripcion())
+    compras = prompt("Que foto queres comprar?");
+    console.log("La foto 1 vale: \n" + foto1.precio+"\n" + "El IVA es: " + factura.detalleIva()+"\n" + "El total es: " + factura.sumaIva())
+    rta = prompt("Queres seguir comprando? Pulsa una tecla, sino \"ESC\" para salir").toUpperCase();
+} while (rta != "ESC");
+
+class Factura {
+    constructor(precio){
+        this.precio = precio;
+    }
+    detalleIva() {
+        return this.precio * 0.21;
+    }
+    sumaIva() {
+        return this.precio * 1.21;
+    }
+}
+
+const factura = new Factura(foto1.precio);
+
+const ciudades = ['Berlin','Buenos Aires','Bogota','Madrid','Rio de Janeiro']
+
+console.log(ciudades.indexOf('Bogota'))
+
+ciudades.splice(1, 1)
+console.log(ciudades)
+
+class Video {
+    constructor(ciudad, precio) {
+        this.id = id;
+        this.ciudad  = ciudad.toUpperCase();
+        this.precio  = parseFloat(precio);
+        this.venta = false;
+    }
+    sumaIva() {
+        this.precio = this.precio * 1.21;
+    }
+}
+
+const videos = [];
+
+videos.push(new Video(1,"Berlin", "60"));
+videos.push(new Video(2,"Buenos Aires", "50"));
+
+for (const video of videos) {
+    console.log(video.ciudad);
+    console.log(video.precio);
+}
+
+for (const video of videos)
+    video.sumaIva();
